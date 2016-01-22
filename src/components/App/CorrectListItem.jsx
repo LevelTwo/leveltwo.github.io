fbc76b415af0f0e9fdb56b208a31c4e100cf488a
LevelTwo <nonamemmvs@hotmail.com>
2016-01-22 00:58:51 -0800

    Fixed leftover residue from merging two commits before 😳.

--- src/components/App/CorrectListItem.jsx
+++ src/components/App/CorrectListItem.jsx
@@ -4,11 +4,7 @@ import CheckIcon from 'material-ui/lib/svg-icons/navigation/check'
 export default class CorrectListItem extends Component {
 
   static propTypes = {
-<<<<<<< HEAD
     primaryText: PropTypes.string.isRequired,
-=======
-    label: PropTypes.string.isRequired,
->>>>>>> 3fc8605ccb9c55698b863dbdcc28e9b00fb0124a
   }
 
   static contextTypes = {
@@ -20,11 +16,7 @@ export default class CorrectListItem extends Component {
 
     return (
       <div>
-<<<<<<< HEAD
         <span style={{verticalAlign: "middle", paddingRight: 10, color: accent2Color}}>{this.props.primaryText}</span>
-=======
-        <span style={{verticalAlign: "middle", paddingRight: 10, color: accent2Color}}>{this.props.label}</span>
->>>>>>> 3fc8605ccb9c55698b863dbdcc28e9b00fb0124a
         <CheckIcon color={accent2Color} style={{verticalAlign: "middle"}} />
       </div>
     )
--- src/components/App/IncorrectListItem.jsx
+++ src/components/App/IncorrectListItem.jsx
@@ -4,11 +4,7 @@ import CloseIcon from 'material-ui/lib/svg-icons/navigation/close'
 export default class IncorrectListItem extends Component {
 
   static propTypes = {
-<<<<<<< HEAD
     primaryText: PropTypes.string.isRequired,
-=======
-    label: PropTypes.string.isRequired,
->>>>>>> 3fc8605ccb9c55698b863dbdcc28e9b00fb0124a
   }
 
   static contextTypes = {
@@ -20,11 +16,7 @@ export default class IncorrectListItem extends Component {
 
     return (
       <div>
-<<<<<<< HEAD
         <span style={{verticalAlign: "middle", paddingRight: 10, color: accent3Color}}>{this.props.primaryText}</span>
-=======
-        <span style={{verticalAlign: "middle", paddingRight: 10, color: accent3Color}}>{this.props.label}</span>
->>>>>>> 3fc8605ccb9c55698b863dbdcc28e9b00fb0124a
         <CloseIcon color={accent3Color} style={{verticalAlign: "middle"}} />
       </div>
     )
